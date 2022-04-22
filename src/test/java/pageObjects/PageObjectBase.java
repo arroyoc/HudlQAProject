370abcdeccd69287b501d808639b6fe7a6692d40
Chris Arroyo <arroyoc91@gmail.com>
2022-04-22 10:06:12 -0600

    login page page objects

--- src/test/java/pageObjects/PageObjectBase.java
+++ src/test/java/pageObjects/PageObjectBase.java
@@ -11,49 +11,48 @@ import java.util.List;
 
 public class PageObjectBase {
 
-    public class PageObjectsBase {
+    protected WebDriver driver;
+    private int timeout = 15;
+    private int elementExistsTimeout = 5;
 
-        protected WebDriver driver;
-        private int timeout = 15;
-        private int elementExistsTimeout = 5;
+    public PageObjectBase(WebDriver wdriver) {
+        this.driver = wdriver;
+    }
 
-        public PageObjectsBase(WebDriver wdriver) {
-            this.driver = wdriver;
+    public WebElement waitForElement(By locator) {
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        public WebElement waitForElement(By locator) {
-            WebDriverWait wait = new WebDriverWait(driver, timeout);
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+        return driver.findElement(locator);
+    }
+
+    public WebElement waitAndClick(By locator) {
+        WebElement element = waitForElement(locator);
+        element.click();
+        return element;
+    }
+
+    public boolean elementExists(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, elementExistsTimeout);
             wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-            return driver.findElement(locator);
+        } catch (TimeoutException e) {
+            return false;
         }
 
-        public WebElement waitAndClick(By locator) {
-            WebElement element = waitForElement(locator);
-            element.click();
-            return element;
-        }
+        return true;
+    }
 
-        public List<WebElement> waitForElements(By locator) {
-            WebDriverWait wait = new WebDriverWait(driver, timeout);
-            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-            return driver.findElements(locator);
-        }
-
-        public WebElement waitForElementToBeVisible(By locator) {
-            WebDriverWait wait = new WebDriverWait(driver, timeout);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-            return driver.findElement(locator);
-        }
-
-        public boolean elementExists(By locator) {
-            try {
-                WebDriverWait wait = new WebDriverWait(driver, elementExistsTimeout);
-                wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-            } catch (TimeoutException e) {
-                return false;
-            }
-
-            return true;
-        }
+    public WebElement waitForElementToBeVisible(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return driver.findElement(locator);
     }
 }
